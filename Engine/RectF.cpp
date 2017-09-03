7b8b46594561078b1d6403e44daf621b3f3d34e4
FatteusHornblow <guitargeek70@gmail.com>
2017-09-03 11:35:52 -0400

    fixed constructors to all use initialzation lists

--- Engine/RectF.cpp
+++ Engine/RectF.cpp
@@ -10,25 +10,29 @@ RectF::RectF(float left_in, float right_in, float top_in, float bottom_in)
 }
 
 RectF::RectF(const Vec2 & top_left, const Vec2 & bottom_right)
+	:
+	RectF(top_left.x, bottom_right.x, top_left.y, bottom_right.y)
 {
-	RectF(top_left.x, bottom_right.x, top_left.y, bottom_right.y);
 }
 
 RectF::RectF(const Vec2 & top_left, float width, float height)
+	:
+	RectF(top_left, top_left + Vec2(width, height))
 {
-	RectF(top_left, Vec2(width, height));
 }
 
 bool RectF::IsOverlappingWith(const RectF& other) const
 {
 	return left <= other.right &&
-		right >= other.left &&
-		top <= other.bottom &&
-		bottom <= other.top;
+		   right >= other.left &&
+		   top <= other.bottom &&
+		   bottom <= other.top;
 }
 
 RectF RectF::FromCenter(const Vec2& center, float halfWidth, float halfHeight)
 {
-	Vec2 half(halfWidth, halfHeight);
+	const Vec2 half(halfWidth, halfHeight);
 	return RectF(center - half, center + half);
 }
+
+
